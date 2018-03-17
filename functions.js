332c046c037b83bdac3e555a3a5fe49c330bdefb
Michael A. Risley <michael.risley@gmail.com>
2018-03-17 09:53:24 -0500

    adding functions

---
+++ functions.js
@@ -0,0 +1,13 @@
+// a good function takes an input and gives an output, without modifying the input
+
+function sayhi(name, name2) {
+	return "Hi " + name + " and " + name2
+}
+
+console.log(sayhi("Zach", "Michael"))
+
+// for-each function
+
+function forEach(collection, func) {
+	
+}
